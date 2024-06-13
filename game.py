4770492335edda1a6b7c15103a477bc7c21f4a98
Alan Rodriguez <gheryk90@gmail.com>
2024-06-13 12:07:45 +0200

    mystery ship appears!

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-import pygame
+import pygame, random
 from spaceship import Spaceship
 from obstacle import Obstacle
 from obstacle import grid
 from alien import Alien
+from laser import Laser
+from alien import MysteryShip
 
 class Game:
     def __init__(self, screen_width, screen_height):
@@ -14,6 +16,8 @@ class Game:
         self.aliens_group = pygame.sprite.Group()
         self.create_aliens()
         self.aliens_direction = -1
+        self.alien_lasers_group = pygame.sprite.Group()
+        self.mystery_ship_group = pygame.sprite.GroupSingle()
 
 
     def create_obstacles(self):
@@ -59,3 +63,13 @@ class Game:
         if self.aliens_group:
             for alien in self.aliens_group.sprites():
                 alien.rect.y += distance
+
+
+    def alien_shoot_laser(self):
+        if self.aliens_group.sprites():
+            random_alien = random.choice(self.aliens_group.sprites())
+            laser_sprite = Laser(random_alien.rect.center, -6, self.screen_height)
+            self.alien_lasers_group.add(laser_sprite)
+
+    def create_mystery_ship(self):
+        self.mystery_ship_group.add(MysteryShip(self.screen_width))
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 from game import Game
 
 pygame.init()
@@ -15,16 +15,31 @@ clock = pygame.time.Clock()
 
 game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+SHOOT_LASER = pygame.USEREVENT
+pygame.time.set_timer(SHOOT_LASER, 300)
+
+
+MYSTERYSHIP = pygame.USEREVENT + 1
+pygame.time.set_timer(MYSTERYSHIP, random.randint(4000,8000))
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+		if event.type == SHOOT_LASER:
+			game.alien_shoot_laser()
+
+		if event.type == MYSTERYSHIP:
+			game.create_mystery_ship()
+			pygame.time.set_timer(MYSTERYSHIP, random.randint(4000, 8000))
 
 
 	#Updating 
 	game.spaceship_group.update()
+	game.move_aliens()
+	game.alien_lasers_group.update()
+	game.mystery_ship_group.update()
 
 	#Drawing 
 	screen.fill(GREY)
@@ -33,7 +48,8 @@ while True:
 	for obstacle in game.obstacles:
 		obstacle.blocks_groups.draw(screen)
 	game.aliens_group.draw(screen)
-	game.move_aliens()
+	game.alien_lasers_group.draw(screen)
+	game.mystery_ship_group.draw(screen)
 
 	pygame.display.update()
 	clock.tick(60)

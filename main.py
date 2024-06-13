43a5cd07847b77aa022d46514096ddb5d5f76a04
Alan Rodriguez <gheryk90@gmail.com>
2024-06-13 10:55:38 +0200

    shoot lasers and delay lasers

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -26,10 +27,11 @@ while True:
 
 	#Updating 
 	spaceship_group.update()
-	
+
 	#Drawing 
 	screen.fill(GREY)
 	spaceship_group.draw(screen)
+	spaceship_group.sprite.lasers_group.draw(screen)
 	
 	pygame.display.update()
 	clock.tick(60)
---
+++ obstacle.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Block(pygame.sprite.Sprite):  
+	def __init__(self, x, y):
+		self.image = pygame.Surface((3,3))
+		self.image.fill((243, 216, 63))
+		self.rect = self.image.get_rect( topleft = (x,y))
+
+
